d1a5bf6a8985d6a6dab7c6365081c4c10bc32a8b
Claudio Soares <claudiolibanez@gmail.com>
2023-03-10 14:41:37 -0300

    feat: add firebase analytics and remote config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  View,
+  Pressable,
+  Text
+} from 'react-native';
+import remoteConfig from '@react-native-firebase/remote-config';
 
 export default function App() {
+  const [showButtonFlag, setShowButtonFlag] = useState(false);
+  const [titleButtonText, setTitleButtonText] = useState('');
+
+  async function fetchRemoteConfig() {
+    await remoteConfig()
+      .setConfigSettings({
+        minimumFetchIntervalMillis: 3000, // 3 seconds; default is 12 hours
+      });
+
+    await remoteConfig().setDefaults({
+      show_button_toggle: false,
+      title_button_text: '',
+    });
+
+    await remoteConfig().fetchAndActivate();
+
+    // const showButton = remoteConfig()
+    //   .getValue('show_button_toggle')
+    //   .asBoolean();
+
+    const { show_button_toggle, title_button_text } = remoteConfig().getAll();
+
+    setShowButtonFlag(show_button_toggle.asBoolean());
+    setTitleButtonText(title_button_text.asString());
+  }
+
+  useEffect(() => {
+    fetchRemoteConfig();
+  }, []);
+
   return (
     <>
-      <StatusBar style="auto" />
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+          <Text>Hello World!</Text>
+        </View>
+        {showButtonFlag && (
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.button}>
+              <Text style={styles.buttonText}>{titleButtonText}</Text>
+            </Pressable>
+          </View>
+        )}
+      </SafeAreaView>
     </>
   );
 }
@@ -16,7 +67,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  buttonContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+  },
+  button: {
+    backgroundColor: '#000',
+    padding: 12,
+    borderRadius: 4,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
   },
 });
